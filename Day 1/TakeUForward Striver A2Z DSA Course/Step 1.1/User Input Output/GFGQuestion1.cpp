a062e539147b897a02a21420d8d31293ba1ae764
Shekhar Sakat <shekharsakat801@gmail.com>
2025-12-20 17:12:50 +0530

    Add files via upload

---
+++ Day 1/TakeUForward Striver A2Z DSA Course/Step 1.1/User Input Output/GFGQuestion1.cpp
@@ -0,0 +1,33 @@
+/*C++ Input / Output
+Difficulty: BasicAccuracy: 68.77%Submissions: 66K+Points: 1
+Given two integers A and B. Your task is to return their product.
+
+ 
+
+Example 1:
+
+Input:
+A = 1, B = 2
+Output:
+2
+Explanation:
+Multiplication of 1 and 2 is 2.
+ 
+
+Example 2:
+
+Input:
+A = 8, B = 7
+Output:
+56
+Explanation:
+Multiplication of 8 and 7 is 56.*/
+
+
+class Solution {
+  public:
+    int multiplication(int A, int B) {
+        // code here
+            return A * B;
+    }
+};

cb4014a2f5f8e624c60bbaf86d432d2e605236a1
Shekhar Sakat <shekharsakat801@gmail.com>
2025-12-22 20:32:47 +0530

    Add files via upload

---
+++ Day 2/C++/Literals/FloatingPointLiterals.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float a = 10.5f;
+    double b = 10.515;
+    float c = 2.1e15f; //2.1 * 10 ^15
+    double d = 200.1e-80; ///200.1 * 10 ^ 80
+    double f = 0x1A.1p2;
+
+    cout << a << "\n" ;
+    cout << b << "\n" ;
+    cout << c << "\n" ;
+    cout << d << "\n" ;
+    cout << f << "\n" ;
+    return 0;
+}
